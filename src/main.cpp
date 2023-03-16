5b8e8d147de39f6458ad2c3bdcc6a20830a130e5
Tauno Erik <sedumacre@gmail.com>
2023-03-16 12:18:13 +0200

    first lamp test

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 /*
 File: main.cpp
 Tauno Erik
-10.03.2023
+16.03.2023
 https://taunoerik.art/
 
 Hardware:
   Raspberry Pi Pico
   LD2410 Radar Sensor
-  WS2812B RGB LED strip
+  WS2812B RGB LED strip, 60 LED/m, 0.8 m ringis
 
 Pins:
 Pico GPIO0 (TX) -> Radar RX
@@ -20,20 +20,20 @@ Pico GND        -> Radar GND
 #include "LD2410.h"             // https://github.com/Renstec/LD2410/
 #include <Adafruit_NeoPixel.h>  // https://github.com/adafruit/Adafruit_NeoPixel/blob/master/examples/strandtest_nodelay/strandtest_nodelay.ino
 
+#define DEBUG
+#include "utils_debug.h"
+
 #define RADAR_BAUD 256000
 #define USB_BAUD   115200
 
 // Pins:
-//const uint8_t RADAR_RX_PIN = 1;  // Allready default RX pin
-//const uint8_t RADAR_TX_PIN = 0;  // Allready default TX pin
-const int LED_1_PIN = 21;
-const int LED_2_PIN = 20;
-const int LED_3_PIN = 19;
-const int LED_4_PIN = 18;
-const int RGB_PIN   = 15;
+//const int RADAR_RX_PIN = 0;  // Allready default RX pin
+//const int RADAR_TX_PIN = 1;  // Allready default TX pin
+const int RADAR_OUT_PIN = 2;  // GP2
+const int RGB_IN_PIN   = 22;  // GP22
 
-// RGB
-const int NUM_OF_RGBS = 80;
+// RGB LEDs
+const int NUM_OF_LEDS = 49;
 
 int rgb_R = 0;
 int rgb_G = 0;
@@ -42,8 +42,6 @@ int rgb_B = 0;
 int      pixel_interval = 50;  // Pixel Interval (ms)
 uint16_t pixel_current = 0;    // Pattern Current Pixel Number
 int      pixel_cycle = 0;      // Pattern Pixel Cycle
-
-Adafruit_NeoPixel RGB_strip(NUM_OF_RGBS, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
 // Time
 bool is_movement = false;
@@ -54,19 +52,13 @@ int time_px_prev = 0;  // Millis
 int px_interval = 50;  // Millis
 int no_movment_time = 0;
 
-// LEDS
-const int num_of_leds = 4;
-int leds[num_of_leds] = {
-  LED_1_PIN, LED_2_PIN, LED_3_PIN, LED_4_PIN
-};
+// Init RGB strip
+Adafruit_NeoPixel RGB_strip(NUM_OF_LEDS, RGB_IN_PIN, NEO_GRB + NEO_KHZ800);
 
-// Radar
+// Init Radar
 LD2410 radar(Serial1);
 
-// True enables Radar Enginering Mode
-bool is_radar_eng_bode = false;
-
-// RGB Funcs
+bool is_radar_eng_mode = false;  // True enables Radar Enginering Mode
 
 
 // Input: 0 to 255 to get a color value.
@@ -94,7 +86,7 @@ uint32_t Wheel(uint8_t wheel_pos) {
 // (as a single 'packed' 32-bit value, which you can get by calling
 // strip.Color(red, green, blue) as shown in the loop() function above),
 // and a delay time (in milliseconds) between pixels.
-void colorWipe(uint32_t color, int wait) {
+void Rgb_color_wipe(uint32_t color, int wait) {
 
   if(pixel_interval != wait) {
     pixel_interval = wait;                   //  Update delay time
@@ -104,7 +96,7 @@ void colorWipe(uint32_t color, int wait) {
   RGB_strip.show();
   pixel_current++;
 
-  if(pixel_current >= NUM_OF_RGBS) {
+  if(pixel_current >= NUM_OF_LEDS) {
     pixel_current = 0;                               //  Loop the pattern from the first LED
   }
 
@@ -119,7 +111,7 @@ void rainbow(uint8_t wait) {
     pixel_interval = wait;
   }
 
-  for(uint16_t i=0; i < NUM_OF_RGBS; i++) {
+  for(uint16_t i=0; i < NUM_OF_LEDS; i++) {
     RGB_strip.setPixelColor(i, Wheel((i + pixel_cycle) & 255)); //  Update delay time
   }
 
@@ -133,20 +125,21 @@ void rainbow(uint8_t wait) {
 
 
 void setup() {
+  // Start USB serial print
   Serial.begin(USB_BAUD);
   delay(1000);
-  Serial.println("LD2410-Radar Example");
+
+  // Start UART to RADAR
+  Serial1.begin(RADAR_BAUD);
   delay(1000);
 
-  Serial1.begin(RADAR_BAUD);  // UART1 to radar
+  Serial.println("Art Light started!");
   delay(1000);
-
-  for(int i = 0; i < num_of_leds; i++) {
-    pinMode(leds[i], OUTPUT);
-  }
 
   // Restore radar default values
   //bool is_radar_factory_reset = radar.factoryReset();
+  //Serial.print("Factory reset: ");
+  //Serial.println(is_radar_factory_reset);
 
   // Change radar baud rate:
   // BAUD_9600, BAUD_19200, BAUD_38400, BAUD_57600,
@@ -196,20 +189,22 @@ void setup() {
 
   Serial.println();
 
-  radar.enableEngMode(is_radar_eng_bode);  // If true enables
+  // Enable or disable Radar Engineering mode
+  radar.enableEngMode(is_radar_eng_mode);
 
-  // 
+  // RGB strip
+  
   RGB_strip.begin();
-  RGB_strip.show();            // Turn OFF all pixels ASAP
-  RGB_strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  RGB_strip.show();             // Turn OFF all pixels ASAP
+  RGB_strip.setBrightness(50);  // Set BRIGHTNESS to about 1/5 (max = 255)
  
   randomSeed(analogRead(0));
-  rainbow(3);
-  colorWipe(RGB_strip.Color(0, 0, 0), 0);
-
+  
+  Serial.println("Setup finished!");
 }
 
 void loop() {
+  /*
   RGB_strip.clear(); // Set all pixel colors to 'off'
   rainbow(10); // Flowing rainbow cycle along the whole strip
   colorWipe(RGB_strip.Color(255, 0, 0), 50);  // Red
@@ -222,6 +217,7 @@ void loop() {
   RGB_strip.setPixelColor(1, RGB_strip.Color(150, 0, 0));
   RGB_strip.setPixelColor(2, RGB_strip.Color(0, 0, 150));
   RGB_strip.show();   // Send the updated pixel colors to the hardware.
+  */
   
   // read must be called cyclically
   if (radar.read()) {
@@ -237,25 +233,17 @@ void loop() {
         break;
       case 0x01:
         Serial.println(" moving target detected");
+        Rgb_color_wipe(RGB_strip.Color(0, 0, 255), 50);
         break;
       case 0x02:
         Serial.println(" stationary target detected");
+        Rgb_color_wipe(RGB_strip.Color(0, 255, 0), 50);
         break;
       case 0x03:
         Serial.println(" moving and stationary target detected");
+        Rgb_color_wipe(RGB_strip.Color(255, 0, 0), 50);
         break;
     }
-
-    for (int i = 0; i < num_of_leds; i++) {
-      if (i == radar.cyclicData.targetState) {
-        Serial.println("High");
-        digitalWrite(leds[i], HIGH);
-      } else {
-        Serial.println("Low");
-        digitalWrite(leds[i], LOW);
-      }
-    }
-
 
     Serial.print("Moving taget distance in cm: ");
     Serial.println(radar.cyclicData.movingTargetDistance);
